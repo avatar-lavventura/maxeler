dea754a6fcafeb18f1d7fa6924e432ef92abc1e3
erdemorman90 <erdemorman90@gmail.com>
2013-03-20 21:47:58 +0000

    Finished prefix sum

---
+++ PrefixSum/CPUCode/PrefixSumCPUCode.c
@@ -0,0 +1,40 @@
+#include "Maxfiles.h"
+
+#define SIZE 16
+
+void prefix_sum_cpu(long *dataIn, long *dataOut, int size) {
+	dataOut[0] = dataIn[0];
+	for(int i = 1; i < size; i++) {
+		dataOut[i] = dataIn[i] + dataOut[i - 1];
+	}
+}
+
+int main() {
+	long dataIn[SIZE], dataOut[SIZE], dataExpected[SIZE];
+
+	// initialize dataIn
+	for(int i = 0; i < SIZE; i++) {
+		dataIn[i] = i + 1;
+	}
+
+	// calculate expected result
+	prefix_sum_cpu(dataIn, dataExpected, SIZE);
+
+	printf("Executing...\n");
+
+	PrefixSum(SIZE, dataIn, dataOut);
+
+	// print result
+	for(int i = 0; i < SIZE; i++) {
+		if(dataOut[i] == dataExpected[i]) {
+			printf("dataExpected[%d] = dataOut[%d] = %ld\n", i, i, dataOut[i]);
+		} else {
+			fprintf(stderr, "dataExpected[%d] = %ld\t\tdataOut[%d] = %ld\n",
+					i, dataExpected[i], i, dataOut[i]);
+		}
+	}
+
+	printf("End of execution...\n");
+
+	return 0;
+}
