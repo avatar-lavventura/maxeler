80f2064818cab74fe6268017d3e836b3e7c243c8
erdemorman90 <erdemorman90@gmail.com>
2013-04-02 18:21:36 +0000

    refactored all code

--- ParallelPrefixSum/CPUCode/ParallelPrefixSumCPUCode.c
+++ ParallelPrefixSum/CPUCode/ParallelPrefixSumCPUCode.c
@@ -3,40 +3,46 @@
 #include <stdio.h>
 #include "Maxfiles.h"
 
-#define SIZE 1073741824
+#define SIZE 1024
+#define DEBUG 1
 
-void prefix_sum_cpu(uint32_t *dataIn, uint32_t *dataOut, int size) {
+void ParallelPrefixSumCPU(int64_t *dataIn, int64_t *dataOut, size_t size) {
 	dataOut[0] = dataIn[0];
-	for(int i = 1; i < size; i++) {
+	for (size_t i = 1; i < size; i++) {
 		dataOut[i] = dataIn[i] + dataOut[i - 1];
 	}
 }
 
 int main() {
-	size_t sizeInBytes = SIZE * sizeof(uint32_t);
-	uint32_t *dataIn = malloc(sizeInBytes);
-	uint32_t *dataOut = malloc(sizeInBytes);
-	uint32_t *dataExpected = malloc(sizeInBytes);
+	size_t dataSizeInBytes = SIZE * sizeof(int64_t);
+	int64_t *dataIn = malloc(dataSizeInBytes);
+	int64_t *dataOut = malloc(dataSizeInBytes);
+	int64_t *dataExpected = malloc(dataSizeInBytes);
 
-	for(int i = 0; i < SIZE; i++) {
-		dataIn[i] = i+1;
+	for (int i = 0; i < SIZE; i++) {
+		dataIn[i] = i + 1;
 		dataOut[i] = 0;
 	}
-	printf("Running CPU \n");
-	prefix_sum_cpu(dataIn, dataExpected, SIZE);
 
-	printf("Running DFE.\n");
+	if (DEBUG == 1) {
+		printf("Running CPU...\n");
+		ParallelPrefixSumCPU(dataIn, dataExpected, SIZE);
+	}
+
+	printf("Running DFE...\n");
 	ParallelPrefixSum(SIZE, dataIn, dataOut);
-	for(int i = 0; i < SIZE; i++) {
-			if(dataOut[i] == dataExpected[i]) {
-				printf("dataExpected[%d] = dataOut[%d] = %d\n", i, i, dataOut[i]);
+	printf("End of DFE...\n");
+	if (DEBUG == 1) {
+		for (int i = 0; i < SIZE; i++) {
+			if (dataOut[i] == dataExpected[i]) {
+				printf("dataExpected[%d] = dataOut[%d] = %ld\n", i, i,
+						dataOut[i]);
 			} else {
-				printf("dataExpected[%d] = %d\t\tdataOut[%d] = %d\n",
-						i, dataExpected[i], i, dataOut[i]);
+				printf("dataExpected[%d] = %ld\t\tdataOut[%d] = %ld\n", i,
+						dataExpected[i], i, dataOut[i]);
 			}
 		}
-	printf("End of DFE.\n");
-
+	}
 
 	free(dataIn);
 	free(dataOut);
@@ -44,4 +50,3 @@ int main() {
 
 	return 0;
 }
-
